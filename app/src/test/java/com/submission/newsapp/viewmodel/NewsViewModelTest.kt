7dff52df90c9336d3f5190d90e92a7703ac51e2a
Muhammad Yoga Alifianto <yoga.alifianto@ist.id>
2023-09-29 10:30:43 +0700

    add to github

--- app/src/test/java/com/submission/newsapp/viewmodel/NewsViewModelTest.kt
+++ app/src/test/java/com/submission/newsapp/viewmodel/NewsViewModelTest.kt
@@ -1,18 +1,6 @@
 package com.submission.newsapp.viewmodel
 
-import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.lifecycle.MutableLiveData
-import com.submission.gameapp.BuildConfig
-import com.submission.newsapp.data.source.remote.network.ApiResponse
-import com.submission.newsapp.data.source.remote.network.RetrofitBuilder
-import com.submission.newsapp.model.BaseSource
-import org.junit.Rule
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.runTest
-import org.junit.Test
-import org.mockito.Mock
-import org.mockito.Mockito.`when`
 
 @ExperimentalCoroutinesApi
 class NewsViewModelTest {
